a17752dcc33703da1cedf8e108056f8b69206cbd
Rafa Rodríguez Galván <rafael.rodriguez@uca.es>
2021-10-24 17:59:46 +0200

    Taller 2: valor abs

--- docs/taller_2.md
+++ docs/taller_2.md
@@ -45,4 +45,62 @@ Para representar números reales no enteros se utiliza la sistema de representac
 
 El tipo de dato flotante en lenguaje C utiliza dos tamaños: `float` (simple precisión) `double` (doble precisión), que ocupan 4 bytes y 8 bytes respectivamente. Puedes consultar información concreta sobre el almacenamiento de [números en coma flotante, recogida en el estándar IEEE 754-2008](https://es.wikipedia.org/wiki/IEEE_754). Comprobarás que:
 
--
+
+
+    #include <stdio.h>
+
+    int main()
+    {
+      int repetir_test = 1;
+      float x, y;
+      float abs_x, abs_y;
+
+      while (repetir_test) {
+        /* Leer dos números reales */
+        printf("Introduce un número real, x: ");
+        scanf("%f", &x);
+        printf("Introduce un número real, y: ");
+        scanf("%f", &y);
+
+        /* Calcular el valor absoluto de x */
+        if(x>0) {
+          abs_x = x;
+        }
+        else {
+           abs_x = -x;
+        }
+
+        /* Calcular el valor absoluto de y */
+        if(y>0) abs_y = y; else abs_y = -y;
+
+        printf("Comprobamos que se verifican las propiedades del valor absoluto:\n");
+
+        /* Comprobar la primera propiedad de valor absoluto */
+        printf("  1) x=%.2f, |x| = %.2f >= 0",    x, abs_x);
+        printf("     y=%.2f, |y| = %.2f >= 0\n",  y, abs_y);
+        if (x==0)  {
+          printf("     x es cero (luego también |x| es cero)\n");
+        }
+        else {
+          printf("     x no es cero (luego tampoco |x| lo es)\n");
+        }
+        if (x==0)
+          printf("     y es cero (luego también |y| es cero)\n");
+        else
+          printf("     y no es cero (luego tampoco |y| lo es)\n");
+
+        /* Comprobar la segunda propiedad de valor absoluto */
+        float abs_x_mas_y = x+y > 0? x+y : -(x+y);  /* Calcular |x+y| */
+        printf("  2) |x+y| = %.2f es menor o igual que |x|+|y|=%.2f\n",
+        abs_x_mas_y, abs_x + abs_y);
+
+        /* Comprobar la tercera propiedad de valor absoluto */
+        float abs_x_por_y = x*y > 0? x*y : -(x*y);  /* Calcular |x*y| */
+        printf("  3) |x·y| = %f = |x|·|y|\n", abs_x_por_y);
+
+        /* Repetir test o terminar */
+        printf("... introduce 0 para terminar o un entero no nulo para repetir: ");
+        scanf("%d", &repetir_test);
+      }
+
+    }
--- src/taller_2/valor_absoluto.c
+++ src/taller_2/valor_absoluto.c
@@ -1,37 +1,58 @@
 #include <stdio.h>
-#include <assert.h>
 
 int main()
 {
-  int i, abs_i;
-  float x, abs_x;
+  int repetir_test = 1;
+  float x, y;
+  float abs_x, abs_y;
 
-  /* Leer un número entero */
-  printf("Introduce un número entero y un número real (coma flotante): ");
-  scanf("%d %f", &i, &x);
+  while(repetir_test) {
 
-  /* Calcular el valor absoluto de i */
-  if(i>0) {
-    abs_i = i;
+    /* Leer dos números reales */
+    printf("Introduce un número real, x: ");
+    scanf("%f", &x);
+    printf("Introduce un número real, y: ");
+    scanf("%f", &y);
+
+    /* Calcular el valor absoluto de x */
+    if(x>0) {
+      abs_x = x;
+    }
+    else {
+      abs_x = -x;
+    }
+
+    /* Calcular el valor absoluto de y */
+    if(y>0) abs_y = y; else abs_y = -y;
+
+    printf("Comprobamos que se verifican las propiedades del valor absoluto:\n");
+
+    /* Comprobar la primera propiedad de valor absoluto */
+    printf("  1) x=%.2f, |x| = %.2f >= 0,",    x, abs_x);
+    printf("     y=%.2f, |y| = %.2f >= 0\n",  y, abs_y);
+    if (x==0)  {
+      printf("     x es cero (luego también |x| es cero)\n");
+    }
+    else {
+      printf("     x no es cero (luego tampoco |x| lo es)\n");
+    }
+    if (x==0)
+      printf("     y es cero (luego también |y| es cero)\n");
+    else
+      printf("     y no es cero (luego tampoco |y| lo es)\n");
+
+    /* Comprobar la segunda propiedad de valor absoluto */
+    float abs_x_mas_y = x+y > 0? x+y : -(x+y);  /* Calcular |x+y| */
+    printf("  2) |x+y| = %.2f es menor o igual que |x|+|y|=%.2f\n",
+           abs_x_mas_y, abs_x + abs_y);
+
+    /* Comprobar la tercera propiedad de valor absoluto */
+    float abs_x_por_y = x*y > 0? x*y : -(x*y);  /* Calcular |x*y| */
+    printf("  3) |x·y| = %f = |x|·|y|\n", abs_x_por_y);
+
+    /* Repetir test o terminar */
+    printf("... introduce 0 para terminar o un entero no nulo para repetir: ");
+    scanf("%d", &repetir_test);
   }
-  else {
-    abs_i = -i;
-  }
-
-  /* Calcular el valor absoluto de x */
-  if(x>0) {
-    abs_x = x;
-  }
-  else {
-    abs_x = -x;
-  }
-
-  printf("Comprobamos que se verifican las propiedades del valor absoluto:\n");
-  printf("1) |%d| = %d > 0,   |%f| = %f > 0\n", i, abs_i, x, abs_x);
-  assert(abs_i > 0);
-  assert(abs_x > 0);
-
-  /* Imprimir el resultado */
-  printf("Su valor absoluto es %i", abs_i);
 
 }
